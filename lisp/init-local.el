e4b3bc8d344d6812bc1f5bd6249f8af56d098eed
Yaguo Zhou <yaguozhou@126.com>
2020-12-09 21:02:13 +0800

    add ggtags

---
+++ lisp/init-local.el
@@ -0,0 +1,6 @@
+(require-package 'ggtags)
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+              (ggtags-mode 1))))
+(provide 'init-local)
